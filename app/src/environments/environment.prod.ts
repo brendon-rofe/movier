f8da941a5e13a52208e7ed81e4ac86e0de7b6c9a
Brendon Rofe <brofe93@gmail.com>
2026-06-04 09:43:55 +0200

    update: Update api url for prod deploy

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // Replace with your Render API Web Service URL after deployment
-  apiBaseUrl: 'https://movier-api.onrender.com/api',
+  apiBaseUrl: 'https://cinetrack-api-9gdy.onrender.com/api',
   tmdbImageBaseUrl: 'https://image.tmdb.org/t/p',
 };
